7a2b6282928676f54d31088ad0bf7b22171ab97a
Danna <thedannitazz@gmail.com>
2021-12-13 22:32:38 -0500

    Se escriben funciones para encriptar contraseña del usuario

--- models/User.js
+++ models/User.js
@@ -1,12 +1,37 @@
 const { Schema, model } = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
 const userSchema = new Schema({
-  email: String,
-  password: String,
+  email: {
+    type: String,
+    required: [true, 'El email es obligatorio'],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'La contraseña es obligatoria'],
+  },
   roles: {
-    admin: Boolean,
+    ref: 'Role',
+    type: Schema.Types.ObjectId,
   },
   orders: Array,
 });
+
+userSchema.statics.encryptPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const newPass = await bcrypt.hash(password, salt);
+    return newPass;
+  } catch (err) {
+    console.log('En el modelo del usuario', err);
+  }
+};
+
+userSchema.statics.comparePassword = async (password, receivedPassword) => {
+  const isEqual = await bcrypt.compare(password, receivedPassword);
+  return isEqual;
+};
 
 module.exports = model('User', userSchema);
